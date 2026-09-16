53ded6fe55a06243e997b6715ec3d48868dead6d
AdamDJohnstone <adam@myoikos.org>
2026-09-17 08:51:00 +1000

    Update labelScale value for Thai language

--- pot-plant/languages/th.js
+++ pot-plant/languages/th.js
@@ -10,7 +10,7 @@ if (new URLSearchParams(window.location.search).get("lang") === "th") {
 window.NCD_POT_PLANT_LANGUAGES.th = {
   code: "th",
   name: "ไทย",
-  labelScale: 0.58,
+  labelScale: 0.62,
   labelLanguage: "th",
   labelFontFamily: 'Tahoma, "Noto Sans Thai", "Leelawadee UI", sans-serif',
   labelLetterSpacing: 0,
