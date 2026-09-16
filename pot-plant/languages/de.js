757b511a2458c8a26a391f5342e5b95948bb16fa
AdamDJohnstone <adam@myoikos.org>
2026-09-16 18:04:25 +1000

    Change labelScale value in German language file

--- pot-plant/languages/de.js
+++ pot-plant/languages/de.js
@@ -4,7 +4,7 @@ window.NCD_POT_PLANT_LANGUAGES =
 window.NCD_POT_PLANT_LANGUAGES.de = {
   code: "de",
   name: "Deutsch",
-  labelScale: 0.88,
+  labelScale: 0.5,
 
   interface: {
     playgroundTitle: "NCD Blumentopf-Spielwiese",
