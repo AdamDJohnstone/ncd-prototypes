00bb0eb8e535b1595fe3f2c44e895c59675332dd
AdamDJohnstone <adam@myoikos.org>
2026-09-16 17:41:53 +1000

    Update and rename test.js to de.js

---
+++ pot-plant/languages/de.js
@@ -0,0 +1,124 @@
+window.NCD_POT_PLANT_LANGUAGES =
+  window.NCD_POT_PLANT_LANGUAGES || {};
+
+window.NCD_POT_PLANT_LANGUAGES.de = {
+  code: "de",
+  name: "Deutsch",
+
+  interface: {
+    playgroundTitle: "NCD Blumentopf-Spielwiese",
+
+    surveyScores: "Umfragewerte",
+    replaySpiral: "Spirale wiederholen",
+    randomResult: "Zufälliges Ergebnis",
+    closeScoresTest: "Ähnliche Werte testen",
+    balancedResult: "Ausgeglichenes Ergebnis",
+
+    maximumFactor: "Maximumfaktor",
+    minimumFactor: "Minimumfaktor",
+
+    watchGrow: "Beim Wachsen zusehen",
+    watchAgain: "Noch einmal ansehen",
+
+    close: "Schließen",
+    language: "Sprache"
+  },
+
+  qcs: {
+    LR: {
+      name: "Liebevolle Beziehungen",
+      lines: [
+        { text: "Liebevolle", emphasis: true },
+        { text: "Beziehungen", emphasis: false }
+      ],
+      labelScale: 1,
+      heartQuestion: "Gehöre ich hier wirklich dazu?",
+      description:
+        "Liebevolle Beziehungen schaffen eine Gemeinschaft, in der Menschen echte Annahme, Fürsorge und Zugehörigkeit erleben."
+    },
+
+    EL: {
+      name: "Bevollmächtigende Leitung",
+      lines: [
+        { text: "Bevollmächtigende", emphasis: true },
+        { text: "Leitung", emphasis: false }
+      ],
+      labelScale: 1,
+      heartQuestion: "Glaubt ihr wirklich an mich?",
+      description:
+        "Bevollmächtigende Leitung erkennt das von Gott gegebene Potenzial von Menschen und hilft ihnen, in sinnvolle Verantwortung hineinzuwachsen."
+    },
+
+    ES: {
+      name: "Zweckmäßige Strukturen",
+      lines: [
+        { text: "Zweckmäßige", emphasis: true },
+        { text: "Strukturen", emphasis: false }
+      ],
+      labelScale: 1,
+      heartQuestion: "Habe ich hier wirklich Raum zu wachsen?",
+      description:
+        "Zweckmäßige Strukturen schaffen Freiheit, Klarheit und Unterstützung, damit Menschen wachsen und sich fruchtbar einbringen können."
+    },
+
+    GBM: {
+      name: "Gabenorientierte Mitarbeit",
+      lines: [
+        { text: "Gabenorientierte", emphasis: true },
+        { text: "Mitarbeit", emphasis: false }
+      ],
+      labelScale: 1,
+      heartQuestion: "Habe ich wirklich etwas beizutragen?",
+      description:
+        "Gabenorientierte Mitarbeit hilft Menschen zu entdecken, wie Gott sie einzigartig begabt hat, und sinnvolle Möglichkeiten zu finden, sich einzubringen."
+    },
+
+    NOE: {
+      name: "Bedürfnisorientierte Evangelisation",
+      lines: [
+        { text: "Bedürfnisorientierte", emphasis: true },
+        { text: "Evangelisation", emphasis: false }
+      ],
+      labelScale: 1,
+      heartQuestion: "Interessiert ihr euch wirklich für mich?",
+      description:
+        "Bedürfnisorientierte Evangelisation beginnt damit, die Menschen, die Gott uns anvertraut hat, wirklich wahrzunehmen und auf ihre Bedürfnisse einzugehen."
+    },
+
+    IWS: {
+      name: "Inspirierender Gottesdienst",
+      lines: [
+        { text: "Inspirierender", emphasis: true },
+        { text: "Gottesdienst", emphasis: false }
+      ],
+      labelScale: 1,
+      heartQuestion: "Begegne ich Gott hier wirklich?",
+      description:
+        "Inspirierender Gottesdienst hilft Menschen, Gott auf eine Weise zu begegnen, die Glauben und Hoffnung erneuert und zu einer Antwort ermutigt."
+    },
+
+    PS: {
+      name: "Leidenschaftliche Spiritualität",
+      lines: [
+        { text: "Leidenschaftliche", emphasis: true },
+        { text: "Spiritualität", emphasis: false }
+      ],
+      labelScale: 1,
+      heartQuestion: "Vertraue ich Gott wirklich?",
+      description:
+        "Leidenschaftliche Spiritualität wächst dort, wo der Glaube aus einer echten und lebensspendenden Beziehung zu Gott gelebt wird."
+    },
+
+    HSG: {
+      name: "Ganzheitliche Kleingruppen",
+      lines: [
+        { text: "Ganzheitliche", emphasis: true },
+        { text: "Kleingruppen", emphasis: false }
+      ],
+      labelScale: 1,
+      heartQuestion: "Kennt ihr mich wirklich?",
+      description:
+        "Ganzheitliche Kleingruppen schaffen Räume, in denen Menschen wirklich gekannt, unterstützt und zum Wachstum herausgefordert werden."
+    }
+  }
+};
