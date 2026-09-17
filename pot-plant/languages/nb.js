1d9703d69fa3be64d86e0eeba34c73a0cd7659fa
AdamDJohnstone <adam@myoikos.org>
2026-09-17 10:18:30 +1000

    Correct Norwegian spirituality description

--- pot-plant/languages/nb.js
+++ pot-plant/languages/nb.js
@@ -99,7 +99,7 @@ window.NCD_POT_PLANT_LANGUAGES.nb = {
       ],
       heartQuestion: "Stoler jeg virkelig på Gud?",
       description:
-        "Et engasjert trosliv vokser der troen leves ut fra en ekte og livgivende Beziehung til Gud."
+        "Et engasjert trosliv vokser der troen leves ut fra en ekte og livgivende relasjon til Gud."
     },
 
     HSG: {
